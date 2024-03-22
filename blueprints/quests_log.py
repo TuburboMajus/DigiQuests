c70a0566a0cf5bdeb59cbc8b9f589edfa54d285c
PyAxolotl <abdellatifzied.saada@gmail.com>
2024-03-22 11:44:51 +0100

    Fix bug when quests list is empty

--- blueprints/quests_log.py
+++ blueprints/quests_log.py
@@ -64,7 +64,7 @@ def getQuests():
 		else:
 			selected_quest = Quest.storage.get(id=active_quest['quest'])
 
-	selected_quest_tasks = [], selected_quest_key = None
+	selected_quest_tasks = []; selected_quest_key = None
 	if selected_quest is not None:
 		selected_quest_tasks = sorted(Task.storage.list(quest=selected_quest['id']),key=lambda t:t['task_order'])
 		selected_quest_key = [key for key in keys if key['resource'] == selected_quest['id']][0]
